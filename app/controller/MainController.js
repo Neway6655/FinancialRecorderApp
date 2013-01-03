2f7450f156a8d39db6e2a02e8faf272350eb82af
Neway Liu <neway.liu@ericsson.com>
2013-01-03 16:05:03 +0800

    use store.load instead to reload store data, add callback function.

--- app/controller/MainController.js
+++ app/controller/MainController.js
@@ -91,71 +91,28 @@ Ext.define('FinancialRecorderApp.controller.MainController', {
     },
 
     selectAccount: function(){
-      this.fetchLatest();
-    },
-
-    showAccountView: function(userStore){
-      var currentUserName = FinancialRecorderApp.app.getCurrentUser();
-      var currentUser = userStore.queryBy(function(record){
-        var userName = record.get('name');
-        if (userName === currentUserName){
-          return true;
+      Ext.getStore('UserStore').load(function(records, operation, success){
+        console.log('UserStore reload records: ' + records);
+        var currentUserName = FinancialRecorderApp.app.getCurrentUser();   
+        var index;
+        for(index = 0; index < records.length; index ++){
+          if (currentUserName === records[index].data.name){
+            break;
+          }
         }
-      });
-
-      if (currentUser.get(0).data.type === 2){
-        // normal user.
-        this.getAccountDetailView().loadFormRecord(currentUser.get(0));
-        Ext.Viewport.animateActiveItem(this.getAccountDetailView(), this.slideLeftTransition);
-      }else {
-        // administrator.
-        Ext.Viewport.animateActiveItem(this.getAccountView(), this.slideLeftTransition);
-      }
+        var currentUser = records[index];
+        if (currentUser.data.type === 2){
+          // normal user.
+          this.getAccountDetailView().loadFormRecord(currentUser);
+          Ext.Viewport.animateActiveItem(this.getAccountDetailView(), this.slideLeftTransition);
+        }else {
+          // administrator.
+          Ext.Viewport.animateActiveItem(this.getAccountView(), this.slideLeftTransition);
+        }
+      }, this);
     },
 
     backToMainView: function(){
       Ext.Viewport.animateActiveItem(this.getMainView(), this.slideRightTransition);
-    },
-
-    fetchLatest: function() {
-        var store = Ext.getStore('UserStore'),
-            proxy = store.getProxy(),
-            operation;
-
-        operation = Ext.create('Ext.data.Operation', {
-            page: 1,
-            start: 0,
-            model: store.getModel(),
-            limit: store.getPageSize(),
-            action: 'read',
-            filters: store.getRemoteFilter() ? store.getFilters() : []
-        });
-
-        proxy.read(operation, this.onLatestFetched, this);
-    },
-
-    onLatestFetched: function(operation) {
-        var store      = Ext.getStore('UserStore'),
-            oldRecords = store.getData(),
-            newRecords = operation.getRecords(),
-            length     = newRecords.length,
-            toInsert   = [],
-            newRecord, oldRecord, i;
-
-        for (i = 0; i < length; i++) {
-            newRecord = newRecords[i];
-            oldRecord = oldRecords.getByKey(newRecord.getId());
-
-            if (oldRecord) {
-                oldRecord.set(newRecord.getData());
-            } else {
-                toInsert.push(newRecord);
-            }
-
-            oldRecord = undefined;
-        }
-        store.insert(0, toInsert);
-
-        this.showAccountView(store);
     },
 });
